a1bd4fa30c46cfd6f452aa8745f59c7daab02971
Sushrut Dhaka <sushrut2@gmail.com>
2020-11-19 18:28:42 -0800

    completed run_ml_pipeline script

--- section2/src/run_ml_pipeline.py
+++ section2/src/run_ml_pipeline.py
@@ -6,6 +6,7 @@ import json
 
 from experiments.UNetExperiment import UNetExperiment
 from data_prep.HippocampusDatasetLoader import LoadHippocampusData
+from sklearn.model_selection import train_test_split
 
 class Config:
     """
@@ -13,58 +14,54 @@ class Config:
     """
     def __init__(self):
         self.name = "Basic_unet"
-        self.root_dir = r"YOUR DIRECTORY HERE"
+        self.root_dir = "/home/workspace"
         self.n_epochs = 10
         self.learning_rate = 0.0002
         self.batch_size = 8
         self.patch_size = 64
-        self.test_results_dir = "RESULTS GO HERE"
+        self.test_results_dir = "/home/workspace/out "
 
 if __name__ == "__main__":
     # Get configuration
 
-    # TASK: Fill in parameters of the Config class and specify directory where the data is stored and 
-    # directory where results will go
+    
     c = Config()
 
     # Load data
     print("Loading data...")
 
-    # TASK: LoadHippocampusData is not complete. Go to the implementation and complete it. 
+     
     data = LoadHippocampusData(c.root_dir, y_shape = c.patch_size, z_shape = c.patch_size)
 
 
     # Create test-train-val split
-    # In a real world scenario you would probably do multiple splits for 
-    # multi-fold training to improve your model quality
+    
 
     keys = range(len(data))
 
-    # Here, random permutation of keys array would be useful in case if we do something like 
-    # a k-fold training and combining the results. 
+. 
 
     split = dict()
 
-    # TASK: create three keys in the dictionary: "train", "val" and "test". In each key, store
+    # create three keys in the dictionary: "train", "val" and "test". In each key, store
     # the array with indices of training volumes to be used for training, validation 
     # and testing respectively.
-    # <YOUR CODE GOES HERE>
+    train, split['test'] = train_test_split(keys, test_size = 0.15)
+    split['train'], split['val'] = train_test_split(train, test_size = 0.20)
+
 
     # Set up and run experiment
     
-    # TASK: Class UNetExperiment has missing pieces. Go to the file and fill them in
+    
     exp = UNetExperiment(c, split, data)
 
-    # You could free up memory by deleting the dataset
-    # as it has been copied into loaders
-    # del dataset 
 
     # run training
     exp.run()
 
     # prep and run testing
 
-    # TASK: Test method is not complete. Go to the method and complete it
+
     results_json = exp.run_test()
 
     results_json["config"] = vars(c)
