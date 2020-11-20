380b4afe7021aeeb447b0463c188f20f2e2637e9
Sushrut Dhaka <sushrut2@gmail.com>
2020-11-19 18:22:07 -0800

    completed volume_stats script

--- section2/src/utils/volume_stats.py
+++ section2/src/utils/volume_stats.py
@@ -22,10 +22,10 @@ def Dice3d(a, b):
     if a.shape != b.shape:
         raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
 
-    # TASK: Write implementation of Dice3D. If you completed exercises in the lessons
-    # you should already have it.
-    # <YOUR CODE HERE>
-    pass
+    
+    intersection = np.sum((a>0)*(b>0))
+    total = np.sum((a>0)) + np.sum((b>0))
+    return 2.*float(intersection)/float(total)
 
 def Jaccard3d(a, b):
     """
@@ -46,8 +46,8 @@ def Jaccard3d(a, b):
     if a.shape != b.shape:
         raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
 
-    # TASK: Write implementation of Jaccard similarity coefficient. Please do not use 
-    # the Dice3D function from above to do the computation ;)
-    # <YOUR CODE GOES HERE>
 
-    return #
+   
+    intersection = np.sum((a>0)*(b>0))
+    denom = np.sum((a>0)) + np.sum((b>0)) - intersection
+    return float(intersection)/float(denom)    
