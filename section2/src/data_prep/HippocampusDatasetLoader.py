106e6556933f2b38d3f458445b3868779959621b
Sushrut Dhaka <sushrut2@gmail.com>
2020-11-19 16:48:36 -0800

    Removed unnecessary comments from HippocampusDatasetLoader script

--- section2/src/data_prep/HippocampusDatasetLoader.py
+++ section2/src/data_prep/HippocampusDatasetLoader.py
@@ -32,13 +32,10 @@ def LoadHippocampusData(root_dir, y_shape, z_shape):
     out = []
     for f in images:
 
-        # We would benefit from mmap load method here if dataset doesn't fit into memory
-        # Images are loaded here using MedPy's load method. We will ignore header 
-        # since we will not use it
         image, _ = load(os.path.join(image_dir, f))
         label, _ = load(os.path.join(label_dir, f))
 
-        # TASK: normalize all images (but not labels) so that values are in [0..1] range
+        normalize all images (but not labels) so that values are in [0..1] range
         image = image.astype(np.single)/np.max(image)
 
         # We need to reshape data since CNN tensors that represent minibatches
@@ -48,15 +45,14 @@ def LoadHippocampusData(root_dir, y_shape, z_shape):
         # Note that since we feed individual slices to the CNN, we only need to 
         # extend 2 dimensions out of 3. We choose to extend coronal and sagittal here
 
-        # TASK: med_reshape function is not complete. Go and fix it!
+        
         image = med_reshape(image, new_shape=(image.shape[0], y_shape, z_shape))
         label = med_reshape(label, new_shape=(label.shape[0], y_shape, z_shape)).astype(int)
 
-        # TASK: Why do we need to cast label to int?
-        # ANSWER: It is to ensure each pixel is classified into distinct classes and does not remain as a floating point number
+        
 
         out.append({"image": image, "seg": label, "filename": f})
 
-    # Hippocampus dataset only takes about 300 Mb RAM, so we can afford to keep it all in RAM
+    
     print(f"Processed {len(out)} files, total {sum([x['image'].shape[0] for x in out])} slices")
     return np.array(out)
